179ef3be4c30014d2b33fc03f84cd60ae96d0dbc
Jack Green <jgree6661@gmail.com>
2021-07-26 16:56:47 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,237 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4from = New System.Windows.Forms.TextBox()
+        Me.TextBox4authorisation = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(0, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(339, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "what is being requested? (state quantity! one line for one type of item! )" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(329, 328)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(112, 59)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(2, 191)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(281, 120)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(0, 178)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(375, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "where is it going? (address of item) / extra details (keep everything in one line" &
+    "!)"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(461, 2)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(338, 446)
+        Me.ListBox1.TabIndex = 5
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(3, 317)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(280, 21)
+        Me.ComboBox2.TabIndex = 6
+        Me.ComboBox2.Text = "DHL"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(289, 31)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(166, 122)
+        Me.TextBox2.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(343, 2)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(112, 26)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "list of email addresses " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "this is being sent to"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(3, 383)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 55)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "send request " & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "to requesitions"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(3, 357)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {100000, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(120, 20)
+        Me.NumericUpDown1.TabIndex = 10
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {66, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(0, 341)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(77, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "TC NUMBER?"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(2, 27)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(281, 148)
+        Me.TextBox3.TabIndex = 12
+        '
+        'TextBox4from
+        '
+        Me.TextBox4from.Location = New System.Drawing.Point(289, 223)
+        Me.TextBox4from.Multiline = True
+        Me.TextBox4from.Name = "TextBox4from"
+        Me.TextBox4from.Size = New System.Drawing.Size(166, 29)
+        Me.TextBox4from.TabIndex = 13
+        Me.TextBox4from.Text = "ruppertreq@outlook.com"
+        '
+        'TextBox4authorisation
+        '
+        Me.TextBox4authorisation.Font = New System.Drawing.Font("Comic Sans MS", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox4authorisation.Location = New System.Drawing.Point(289, 285)
+        Me.TextBox4authorisation.Multiline = True
+        Me.TextBox4authorisation.Name = "TextBox4authorisation"
+        Me.TextBox4authorisation.PasswordChar = Global.Microsoft.VisualBasic.ChrW(8225)
+        Me.TextBox4authorisation.Size = New System.Drawing.Size(166, 29)
+        Me.TextBox4authorisation.TabIndex = 14
+        Me.TextBox4authorisation.Text = "MarthertheDog61"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(286, 207)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(183, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "email addres (use live or outlook only)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(289, 269)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(137, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "password cipher encrypt on"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4authorisation)
+        Me.Controls.Add(Me.TextBox4from)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4from As TextBox
+    Friend WithEvents TextBox4authorisation As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,217 @@
+﻿Imports System.ComponentModel
+Imports System.Net.Mail
+Imports Microsoft.Office.Interop.Word
+Public Class Form1
+    Dim dater = TimeOfDay
+    Dim from
+    Dim jerry
+    Dim objdoc
+    Function emailbot(email As String) As Action
+        Dim mailer = New Net.Mail.SmtpClient
+
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New System.Net.Mail.MailMessage()
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential(TextBox4from.Text, TextBox4authorisation.Text)
+            Smtp_Server.Port = 587
+            Smtp_Server.EnableSsl = True
+            Smtp_Server.Host = "smtp.live.com"
+            Try
+                e_mail.Attachments.Add(New Net.Mail.Attachment("C:\attachment.pdf"))
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+
+
+            e_mail = New System.Net.Mail.MailMessage()
+            e_mail.From = New MailAddress(TextBox4from.Text)
+            e_mail.To.Add(email)
+            e_mail.Subject = "requestions prototype"
+            e_mail.IsBodyHtml = False
+            e_mail.Body = (jerry)
+            Smtp_Server.Send(e_mail)
+            MsgBox("Mail Sent")
+
+
+        Catch ex As Exception
+
+
+        End Try
+
+    End Function
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboBox2.Items.Add("btbox")
+        ComboBox2.Items.Add("dhl")
+        ComboBox2.Items.Add("collection by engineer")
+        ComboBox2.Items.Add("alt post service (add name of alt service here)")
+
+        Try
+            Dim SR As System.IO.StreamReader = IO.File.OpenText("C:\falcon_requestioins_email.txt")
+            While SR.Peek <> -1
+                TextBox2.Text = (SR.ReadToEnd)
+            End While
+            SR.Close()
+        Catch
+
+        End Try
+        Try
+            Dim SR2 As System.IO.StreamReader = IO.File.OpenText("C:\falcon_options_combobox1.txt")
+            While SR2.Peek <> -1
+                TextBox3.Text = (SR2.ReadToEnd)
+            End While
+            SR2.Close()
+        Catch
+
+        End Try
+        Try
+            Dim SR3 As System.IO.StreamReader = IO.File.OpenText("C:\falcon_extra_detes.txt")
+            While SR3.Peek <> -1
+                TextBox1.Text = (SR3.ReadToEnd)
+            End While
+            SR3.Close()
+        Catch
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Form1_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        Try
+            My.Computer.FileSystem.DeleteFile("C:\falcon_requestioins email.txt")
+            My.Computer.FileSystem.DeleteFile("C:\falcon_extra_detes.txt")
+            My.Computer.FileSystem.DeleteFile("C:\falcon_options_combobox1.txt")
+        Catch
+        End Try
+        Dim SW2 As IO.StreamWriter = IO.File.CreateText("C:\falcon_options_combobox1.txt")
+        Try
+            SW2.Write(TextBox3.Text)
+            SW2.Close()
+
+
+            Dim SW As IO.StreamWriter = IO.File.CreateText("C:\falcon_requestioins_email.txt")
+
+            SW.Write(TextBox2.Text)
+
+            SW.Close()
+
+            Dim SW3 As IO.StreamWriter = IO.File.CreateText("C:\falcon_extra_detes.txt")
+
+            SW3.Write(TextBox1.Text)
+
+            SW3.Close()
+        Catch ex As Exception
+            MsgBox("error" & ex.Message)
+        End Try
+    End Sub
+
+    Function kalcus() As Action
+        Dim objWord As Microsoft.Office.Interop.Word.Application
+        Dim objDoc As Microsoft.Office.Interop.Word.Document
+        Dim objTable As Microsoft.Office.Interop.Word.Table
+        objWord = CreateObject("Word.Application")
+        objWord.Visible = True
+        objDoc = objWord.Documents.Add
+        Dim r As Integer, c As Integer
+        Try
+
+
+
+
+
+            objTable = objDoc.Tables.Add(objDoc.Bookmarks.Item("\endofdoc").Range, 15, 5)
+            objTable.Range.ParagraphFormat.SpaceAfter = 6
+            objTable.Cell(1, 1).Range.Text = "TIME"
+            objTable.Cell(1, 2).Range.Text = "TC.NO:"
+            objTable.Cell(1, 3).Range.Text = "item"
+            objTable.Cell(1, 4).Range.Text = "transport method"
+            objTable.Cell(1, 5).Range.Text = "extra notes/address"
+            Try
+                For r = 2 To 15
+                    objTable.Cell(r, 1).Range.Text = TimeOfDay.ToLocalTime.ToLongTimeString
+                    objTable.Cell(r, 2).Range.Text = ListBox1.Items(3).ToString
+                    objTable.Cell(r, 3).Range.Text = (TextBox3.Lines.GetValue(r - 2))
+                    objTable.Cell(r, 4).Range.Text = (ComboBox2.Text)
+                    objTable.Cell(r, 5).Range.Text = (TextBox1.Lines.GetValue(r - 2))
+                Next
+            Catch
+
+            End Try
+            objTable.Rows.Item(1).Range.Font.Bold = True
+            objTable.Rows.Item(1).Range.Font.Italic = True
+            objTable.Borders.Shadow = True
+
+
+
+        Catch ex As Exception
+            MsgBox(ex.Message & ex.Source & "
+" & ex.HelpLink)
+        Finally
+            Try
+                ' Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatPDF used in test
+
+                objdoc.ExportAsFixedFormat("C:\attachment.pdf", Microsoft.Office.Interop.Word.WdSaveFormat.wdFormatPDF)
+                objdoc.Close(False)
+            Catch ex As Exception
+                MsgBox("finaliser code is bad!      " & ex.Message)
+                Try
+                    objdoc.close()
+                Catch
+                    MsgBox("your really in the shnitzel if you get this error contact jgree6661@gmail.com your computer should be ok unless something tampers with my code")
+                    MsgBox("id like to reasure you that the program has simply failed you your rig is in NO danger but im more concered how you got this far.")
+                End Try
+            End Try
+            End Try
+    End Function
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        ListBox1.Items.Remove(ListBox1.Items)
+
+        Dim emailslist = TextBox2.Text
+        ListBox1.Items.Add(TimeOfDay.ToString)
+        ListBox1.Items.Add(TextBox3.Text)
+        ListBox1.Items.Add(TextBox1.Text)
+        ListBox1.Items.Add("TC NO." & NumericUpDown1.Value)
+        ListBox1.Items.Add("" + ComboBox2.Text)
+        Dim i = 0
+
+        For Each item In ListBox1.Items
+            jerry = (jerry & "
+
+" & item)
+        Next
+        While TextBox2.Lines.Count > i
+            ListBox1.Items.Add(TextBox2.Lines.ElementAt(i))
+            i = i + 1
+        End While
+        If My.Computer.FileSystem.FileExists(("C:\attachment.pdf")) = True Then
+            My.Computer.FileSystem.DeleteFile(("C:\attachment.pdf"))
+        End If
+        kalcus()
+        While My.Computer.FileSystem.FileExists(("C:\attachment.pdf")) = False
+            '// make the buttone wait for kalkus to complete the file And then call the email function bot
+        End While
+        For Each item In ListBox1.Items
+                Try
+
+
+                    emailbot(item)
+                Catch
+                End Try
+
+        Next
+
+
+
+    End Sub
+
+
+End Class
